dc2339ae0ff40804bfc5e433402bac7f1ee59e14
FedericoDM <fd.molina@outlook.com>
2026-01-16 18:06:57 -0600

    Adding skills to page

--- app/page.tsx
+++ app/page.tsx
@@ -180,7 +180,7 @@ icon={
 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
 </svg>
 }
-skills={['scikit-learn', 'xgboost', 'pytorch', 'statsmodels', 'regression', 'classification', 'neural networks', 'forecasting', 'recommendation systems']}
+skills={['scikit-learn', 'xgboost', 'pytorch', 'statsmodels', 'regression', 'classification', 'clustering', 'neural networks', 'forecasting', 'recommendation systems']}
 />
 <SkillCard
 title="NLP & AI"
@@ -198,7 +198,7 @@ icon={
 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
 </svg>
 }
-skills={['AWS Lambda', 'EC2', 'Neptune', 'S3', 'IAM', 'SQS', 'SNS', 'Route53', 'Databricks', 'Docker', 'serverless']}
+skills={['AWS Lambda', 'EC2', 'Neptune', 'RDS', 'DynamoDB', 'S3', 'IAM', 'SQS', 'SNS', 'Route53', 'Databricks', 'Docker', 'serverless']}
 />
 </div>
 </section>
